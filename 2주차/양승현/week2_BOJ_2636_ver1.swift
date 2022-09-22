4b1f0a31c232abfe9d0bc059489ef0ad87d6161e
Yang seung hyun <96910404+SHcommit@users.noreply.github.com>
2022-09-22 16:53:09 +0900

    Update week2_BOJ_2636_ver1.swift

--- 2주차/양승현/week2_BOJ_2636_ver1.swift
+++ 2주차/양승현/week2_BOJ_2636_ver1.swift
@@ -1,12 +1,16 @@
 import Foundation
 
 typealias point = (x: Int,y: Int)
+//방향탐색 상 하 좌 우
 let direction = [(-1,0),(1,0),(0,1),(0,-1)]
 
+//공기와 치즈의 위치를 담기 위한 자료구조 큐
 class Queue {
     var queue = [point]()
     var index = 0
 }
+
+//맵의 전반적인 정보가 담김
 class MapInfo {
     
     let width: Int
@@ -23,18 +27,31 @@ class MapInfo {
     }
     
 }
-/*
-    문제에 대한 주요 알고리즘
+
+/**
+ TODO : 치즈가 녹는 최종 과정을 구하기
+ 
+ - Param info : 맵의 정보 담겨있음
+ - Param time : 시간별 치즈가 녹는 시간 기록
+ - Param curCheese : 현재 치즈 양
+ - Param prevCheese : 이전 치즈 양
+
+ 
+ # Notes: #
+ 1. 문제에 대한 주요 알고리즘
     1. exposedAir를 통해 노출된 공기 처리를 한다.
     2. melting을 통해 외부 공기와 접촉된 치즈를 0으로 만들어 추후 실행될 exposedAir를 통해 녹이게 만든다.
     3. 시간 체크를 한다.
+    -> solution()
  */
 class BOJ_2636 {
+    
     var info = MapInfo()
     var time = -1
     var curCheese = 100001
     var prevCheese = 100001
     
+    //전반적인 알고리즘 로직
     func solution() {
         while isMeltingCheese() {
             exposedAir()
@@ -44,6 +61,7 @@ class BOJ_2636 {
         print("\(time)\n\(prevCheese)")
     }
     
+    //치즈가 녹는 과정
     func melting() {
         curCheese = 0
         var visited = Array(repeating: Array(repeating: false, count: info.width), count: info.height)
@@ -56,6 +74,8 @@ class BOJ_2636 {
             }
         }
     }
+    
+    //바깥공기와 접촉된 치즈를 0으로 처리.
     func meltingCheeseCheck(start p: point, visited: inout [[Bool]]) {
         var q = Queue()
         visited[p.y][p.x] = true
@@ -81,6 +101,7 @@ class BOJ_2636 {
         }
     }
     
+    //치즈가 녹는 과정 구현
     func exposedAir() {
         var q = Queue()
         var visited = Array(repeating: Array(repeating: false, count: info.width), count: info.height)
@@ -106,6 +127,7 @@ class BOJ_2636 {
         
     }
     
+    // 녹을 치즈가 있는가?
     func isMeltingCheese() -> Bool {
         if curCheese != 0 {
             prevCheese = curCheese
@@ -114,6 +136,7 @@ class BOJ_2636 {
         return false
     }
     
+    //맵의 밖으로 방향이 탐색되는가?
     func isOutOfMap(p: point) -> Bool {
         return p.x<0 || p.x > info.width - 1 || p.y<0 || p.y > info.height - 1
     }
